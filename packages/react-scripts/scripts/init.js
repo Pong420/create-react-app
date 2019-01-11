f5fb2a1eeddc02c8059bb24cac3a70ffadb6bb50
Pong <samfunghp@gmail.com>
2019-01-11 15:22:41 +0800

    add hot reload and, upgrade react verison and move router to dep

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -99,6 +99,7 @@ module.exports = function(
     build: 'react-scripts build',
     test: 'react-scripts test',
     eject: 'react-scripts eject',
+    component: 'react-scripts component',
   };
 
   // Setup the eslint config
